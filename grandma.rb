9d6c63ceafcd30fb733a3bba9a2ef37c7d3646a0
Sheila Wittman <szandora789@gmail.com>
2018-06-06 21:37:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,21 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+grandma = "I love you grandma!".upcase
+
+if grandma == "whatever you say to grandma"
+  puts "Huh?! Speak up, sonny!".upcase
+elsif grandma == "she thinks she can hear you"
+  puts "No, not since 1938!".upcase
+else grandma == "I love you grandma!".upcase
+  puts "I love you too pumpkin!".upcase
+end
